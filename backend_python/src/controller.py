20448766514a42e7fccb87514351c0abbc7e4913
jnicolasdz <jndiazs@udistrital.edu.co>
2025-02-11 21:53:50 -0500

    setting CORS protocol and deleting unnecesaries test files

--- backend_python/src/controller.py
+++ backend_python/src/controller.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 from Services.Folder import router
 
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost",  
+    "http://localhost:9090",  
+    "http://localhost:8000",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 app = FastAPI(
-    title='Condor API',
-    description='This is the API of the Condor project',
+    title='Two Drive API',
+    description='This is the API of Two Drive to manage folders and files of users',
     version='0.5'
 )
 app.include_router(router)
